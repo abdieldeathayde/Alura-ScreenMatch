4512024d81774b084ff9979a82c0bd6a81a29339
Abdiel Athayde <abdieldeathayde67@gmail.com>
2025-05-10 12:53:44 -0300

    Trabalhando com consumo de API e Records

--- src/main/java/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/main/java/br/com/alura/screenmatch/modelos/Titulo.java
@@ -13,6 +13,12 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
+    }
+
     public String getNome() {
         return nome;
     }
@@ -67,4 +73,15 @@ public class Titulo implements Comparable<Titulo> {
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
+
+    // código omitido…
+
+    @Override
+    public String toString() {
+        return "nome = '" + nome + '\'' +
+                ", anoDeLancamento = " + anoDeLancamento + "," +
+                " duração " + duracaoEmMinutos;
+
+    }
+
 }
--- src/main/java/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/main/java/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,12 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.ietf.jgss.GSSContext;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,5 +30,24 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+//        Gson gson = new Gson();
+//        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+
+
+
+        System.out.println("Título já convertido");
+        System.out.println(meuTitulo);
+
+
     }
 }
